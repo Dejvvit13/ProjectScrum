19fada3423f09403774fdc9c92217b9e3be3057f
Dejvvit13 <92039043+Dejvvit13@users.noreply.github.com>
2022-08-23 09:26:10 +0200

    GUI

--- src/main/java/com/example/threeelementsgamescrum/HelloController.java
+++ src/main/java/com/example/threeelementsgamescrum/HelloController.java
@@ -1,12 +1,17 @@
 package com.example.threeelementsgamescrum;
 
+import javafx.fxml.FXML;
 import javafx.scene.layout.GridPane;
 
 public class HelloController {
 
     private int roundCounter;
+    @FXML
     private GridPane gridPanePC;
+    @FXML
     private GridPane gridPaneUser;
 
 
-}
+
+    }
+
