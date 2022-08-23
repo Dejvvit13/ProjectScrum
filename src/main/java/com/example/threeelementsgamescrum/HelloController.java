2bbfd6294a4606590dcf9548cf4b3fa7e79f4bbf
ajgors <igor.stadnicki@gmail.com>
2022-08-23 13:51:52 +0200

    add event to buttons

--- src/main/java/com/example/threeelementsgamescrum/HelloController.java
+++ src/main/java/com/example/threeelementsgamescrum/HelloController.java
@@ -16,6 +16,10 @@ import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
 
+    public ImageView fightResultPC3;
+    public ImageView fightResultUser3;
+    public ImageView fightResultUser2;
+    public ImageView fightResultPC2;
     private List<String> paths = new ArrayList<>(List.of(
             String.valueOf(this.getClass().getResource("Images/FireElement.png")),
             String.valueOf(this.getClass().getResource("Images/WaterElement.png")),
@@ -105,44 +109,59 @@ public class HelloController implements Initializable {
         }
     }
 
-    @FXML
-    public void onFirstFightButtonClick() {
-
-        String userImageUrl = String.valueOf(imageUser1.getImage().getUrl().substring(139));
-        String pcImageUrl = String.valueOf(imagePC1.getImage().getUrl().substring(139));
-
-// draw
+    public void checkScore(String userImageUrl, String pcImageUrl, ImageView pcImage, ImageView usrImage){
+        // draw
         if (userImageUrl.equals(pcImageUrl)) {
             Image imageDraw = new Image(String.valueOf(this.getClass().getResource("Images/draw.png")));
-            fightResultPC1.setImage(imageDraw);
-            fightResultUser1.setImage(imageDraw);
+            pcImage.setImage(imageDraw);
+            usrImage.setImage(imageDraw);
         }
 //Check water and fire
         if (userImageUrl.equals("WaterElement.png") && pcImageUrl.equals("FireElement.png")) {
             Image imageWin = new Image(String.valueOf(this.getClass().getResource("Images/win.png")));
             Image imageLose = new Image(String.valueOf(this.getClass().getResource("Images/lose.png")));
-            fightResultPC1.setImage(imageLose);
-            fightResultUser1.setImage(imageWin);
+            pcImage.setImage(imageLose);
+            usrImage.setImage(imageWin);
         } else if (userImageUrl.equals("FireElement.png") && pcImageUrl.equals("WaterElement.png")){
             Image imageWin = new Image(String.valueOf(this.getClass().getResource("Images/win.png")));
             Image imageLose = new Image(String.valueOf(this.getClass().getResource("Images/lose.png")));
-            fightResultPC1.setImage(imageWin);
-            fightResultUser1.setImage(imageLose);
+            pcImage.setImage(imageWin);
+            usrImage.setImage(imageLose);
         }
 
         // Check fire and wind
         if (userImageUrl.equals("FireElement.png") && pcImageUrl.equals("WindElement.jpg")) {
             Image imageWin = new Image(String.valueOf(this.getClass().getResource("Images/win.png")));
             Image imageLose = new Image(String.valueOf(this.getClass().getResource("Images/lose.png")));
-            fightResultPC1.setImage(imageLose);
-            fightResultUser1.setImage(imageWin);
+            pcImage.setImage(imageLose);
+            usrImage.setImage(imageWin);
         } else if (userImageUrl.equals("WindElement.jpg") && pcImageUrl.equals("FireElement.png")){
             Image imageWin = new Image(String.valueOf(this.getClass().getResource("Images/win.png")));
             Image imageLose = new Image(String.valueOf(this.getClass().getResource("Images/lose.png")));
-            fightResultPC1.setImage(imageWin);
-            fightResultUser1.setImage(imageLose);
+            pcImage.setImage(imageWin);
+            usrImage.setImage(imageLose);
         }
+    }
+    @FXML
+    public void onFirstFightButtonClick() {
 
+        String userImageUrl = String.valueOf(imageUser1.getImage().getUrl().substring(imageUser1.getImage().getUrl().lastIndexOf('/')+1));
+        String pcImageUrl = String.valueOf(imagePC1.getImage().getUrl().substring(imageUser1.getImage().getUrl().lastIndexOf('/')+1));
+        checkScore(userImageUrl, pcImageUrl, fightResultPC1, fightResultUser1);
+    }
+    @FXML
+    public void onSecondFightButtonClick() {
+
+        String userImageUrl = String.valueOf(imageUser2.getImage().getUrl().substring(imageUser2.getImage().getUrl().lastIndexOf('/')+1));
+        String pcImageUrl = String.valueOf(imagePC2.getImage().getUrl().substring(imageUser2.getImage().getUrl().lastIndexOf('/')+1));
+        checkScore(userImageUrl, pcImageUrl, fightResultPC2, fightResultUser2);
+    }
+    @FXML
+    public void onThirdFightButtonClick() {
+
+        String userImageUrl = String.valueOf(imageUser3.getImage().getUrl().substring(imageUser3.getImage().getUrl().lastIndexOf('/')+1));
+        String pcImageUrl = String.valueOf(imagePC3.getImage().getUrl().substring(imageUser3.getImage().getUrl().lastIndexOf('/')+1));
+        checkScore(userImageUrl, pcImageUrl, fightResultPC3, fightResultUser3);
     }
 
 }
