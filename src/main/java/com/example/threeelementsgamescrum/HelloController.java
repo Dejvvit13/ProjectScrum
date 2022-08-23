25851e450f8073f6f27cafa56abb9e39d345ca14
Dejvvit13 <92039043+Dejvvit13@users.noreply.github.com>
2022-08-23 09:21:41 +0200

    GUI

--- src/main/java/com/example/threeelementsgamescrum/HelloController.java
+++ src/main/java/com/example/threeelementsgamescrum/HelloController.java
@@ -1,14 +1,12 @@
 package com.example.threeelementsgamescrum;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private int roundCounter;
+    private GridPane gridPanePC;
+    private GridPane gridPaneUser;
+
+
 }
