147f20b41ed8b7433f8e0ea46ca0e928f5305664
Dejvvit13 <92039043+Dejvvit13@users.noreply.github.com>
2022-08-23 09:28:56 +0200

    GUI

--- src/main/java/com/example/threeelementsgamescrum/HelloController.java
+++ src/main/java/com/example/threeelementsgamescrum/HelloController.java
@@ -1,17 +1,27 @@
 package com.example.threeelementsgamescrum;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
 
     private int roundCounter;
     @FXML
     private GridPane gridPanePC;
     @FXML
     private GridPane gridPaneUser;
+    @FXML
+    private TextField roundDisplay;
 
 
-
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        roundDisplay.setText("ROUND  " + roundCounter);
     }
+}
 
